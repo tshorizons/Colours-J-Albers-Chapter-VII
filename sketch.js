af122b34754366dd78339748cb6e14ba5cf52a0e
tshorizons <123700523+tshorizons@users.noreply.github.com>
2023-09-19 18:01:55 +0100

    Commit translate image to centre

--- sketch.js
+++ sketch.js
@@ -8,23 +8,24 @@ function setup() {
 function draw() {
   colorMode(HSB,360,128,128,[100]);
   background(240,128,128,50);
+  translate (width/2, height/4);
   noStroke();
 
   fill(60,128,128,100);
-  rect (50,50,100,100);
-  rect (50,40,20,90);
+  rect (-170,0,100,100);
+  //rect (-150,0,20,90);
   
   fill(60,128,64,100);
-  rect (170,50,100,100);
+  rect (-50,0,100,100);
   
   fill(60,128,32,50);
-  rect (290,50,100,100);
+  rect (70,0,100,100);
 
  
   fill(35,128,128,75);
-  ellipse(220,150,220,70)
+  ellipse(0,100,220,70)
   fill(50,128,128,75);
-  ellipse(220,200,70,220);
+  ellipse(0,150,70,220);
   
     
   
